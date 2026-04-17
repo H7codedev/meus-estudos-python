6c8281aefbe2719aea19cd50929343682cdcee15
Hector Marcelo <hectorjanete@gmail.com>
2026-04-16 21:27:07 -0300

    Atualizando calculadora com +, - e *

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,14 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-soma = num1 + num2
+operacao = input("Escolha + ou -: ")
 
-print("Resultado da soma :" , soma)
+if operacao == "+":
+    resultado = num1 + num2
+elif operacao == "-":
+    resultado = num1 - num2
+else:
+    print("Operação inválida")
+    exit()
+
+print("Resultado:", resultado)
